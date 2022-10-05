a2de1502b3b15d7bacae2e29d3747557e946b49e
Snygrt <sanyozhik@gmail.com>
2022-10-05 12:32:18 +0300

    1

---
+++ bornyear.py
@@ -0,0 +1,6 @@
+question = input("В каком году родился Пушкин?")
+question = int(question)
+if question != 1799:
+    print("Кажется ответ неверный")
+else:
+    print("Совершенно верно")
---
+++ variables.py
@@ -0,0 +1,8 @@
+subject = "помидор"
+color = "red"
+weight = 0,456
+amount = 3
+isRipe = True
+propertyes = (color, weight, amount, isRipe)
+for var in propertyes:
+    print(var, type(var))
